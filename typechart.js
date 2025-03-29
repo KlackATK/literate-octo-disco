64dbb7b2430dbdc6e1bcd95545bf5f253d145db0
KlackATK <maya.toole@yahoo.com>
2025-03-29 15:09:21 -0500

    Add files via upload

--- typechart.js
+++ typechart.js
@@ -1,7 +1,11 @@
 
 function themeToggle() {
 	document.getElementById("ld-toggle").addEventListener("click", function() {
-		document.body.classList.toggle("light-mode");
 		document.body.classList.toggle("dark-mode");
+		
+		var icon = document.getElementById("icon");
+		icon.classList.toggle("fa-moon");
+		icon.classList.toggle("fa-sun");
+		
 });
 }
